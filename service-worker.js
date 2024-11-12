b609c61b4af46b8927beb63be3c08f43a45b9b8e
Wouter <w.vos@elitacwearable.com>
2024-11-12 08:54:05 +0100

    used autotune to get a flat note

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 
-const CACHE_NAME = "v2.2"; 
+const CACHE_NAME = "v2.3"; 
 
 const putInCache = async (request, response) => {
     const cache = await caches.open(CACHE_NAME);
