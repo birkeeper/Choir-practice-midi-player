0a0850b2fa0714a8d78068fb3ecca15ad80f6c38
Wouter <w.vos@elitacwearable.com>
2024-11-15 20:24:33 +0100

    updated cache version

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 
-const CACHE_NAME = "v2.3"; 
+const CACHE_NAME = "v2.4"; 
 
 const putInCache = async (request, response) => {
     const cache = await caches.open(CACHE_NAME);
