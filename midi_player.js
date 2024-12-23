c3775e4627195477e7801361e80f2c295d726161
Wouter <w.vos@elitacwearable.com>
2024-12-23 18:59:00 +0100

    retry again

--- midi_player.js
+++ midi_player.js
@@ -8,7 +8,7 @@ import {ALL_CHANNELS_OR_DIFFERENT_ACTION} from './libraries/spessasynth_lib/src/
 import { loadSoundFont } from "./libraries/spessasynth_lib/src/spessasynth_lib/soundfont/load_soundfont.js";
 import { getPauseSvg, getPlaySvg, getFileOpenSvg } from './js/icons.js'
 
-const VERSION = "v1.1.2"
+const VERSION = "v1.1.2.1"
 const DEFAULT_PERCUSSION_CHANNEL = 9; // In GM channel 9 is used as a percussion channel
 const ICON_SIZE_PX = 24; // size of button icons
 
@@ -22,7 +22,7 @@ async function generateHash(fileBuffer) {
     const hashArray = Array.from(new Uint8Array(hashBuffer));
     const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
     return hashHex;
-}
+} 
 
 if ("serviceWorker" in navigator) {
     // Register a service worker hosted at the root of the
