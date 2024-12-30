fc6e924acb65a82c4a374a1f095ddec069ac8046
Wouter <w.vos@elitacwearable.com>
2024-12-30 10:57:26 +0100

    updated library. Should fix double event call when song is changed

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 
-const CACHE_NAME = "v4.0"; 
+const CACHE_NAME = "v3.1"; 
 
 const putInCache = async (request, response) => {
     const cache = await caches.open(CACHE_NAME);
