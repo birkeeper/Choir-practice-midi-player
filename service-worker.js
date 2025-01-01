578139c78c3552d3bd32a4e1fa3064fc5b14de5c
Wouter <w.vos@elitacwearable.com>
2025-01-01 18:32:19 +0100

    changed size select element

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 
-const CACHE_NAME = "v4.10"; 
+const CACHE_NAME = "v4.11"; 
 
 const putInCache = async (request, response) => {
     const cache = await caches.open(CACHE_NAME);
