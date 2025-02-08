910d706a6b1b7002a9b16602126fa023b9e57ed4
Wouter <w.vos@elitacwearable.com>
2025-02-08 18:04:12 +0100

    fixed png loading failures

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 
-const CACHE_NAME = "v7.3"; 
+const CACHE_NAME = "v7.4"; 
 
 const putInCache = async (request, response) => {
     const cache = await caches.open(CACHE_NAME);
