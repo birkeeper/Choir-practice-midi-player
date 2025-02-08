a98d47b432a6f4ce77de94f905ca560f05e3de07
Wouter <w.vos@elitacwearable.com>
2025-02-08 17:28:52 +0100

    Added icons for support on iOS and MacOS

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 
-const CACHE_NAME = "v7.0"; 
+const CACHE_NAME = "v7.1"; 
 
 const putInCache = async (request, response) => {
     const cache = await caches.open(CACHE_NAME);
