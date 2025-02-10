3cac7110b1cffb98e3c1ad66f1bc13ebf06a5aa4
Wouter <w.vos@elitacwearable.com>
2025-02-10 15:13:22 +0100

    corrected typo

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 
-const CACHE_NAME = "v7.7"; 
+const CACHE_NAME = "v7.8"; 
 
 const putInCache = async (request, response) => {
     const cache = await caches.open(CACHE_NAME);
