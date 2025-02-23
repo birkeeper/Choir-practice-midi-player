e3b51b454695804285fed0052a2e4b4eeb5b98a0
Wouter <w.vos@elitacwearable.com>
2025-02-23 18:04:21 +0100

    reformat playback

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 
-const CACHE_NAME = "v7.25"; 
+const CACHE_NAME = "v7.26"; 
 
 const putInCache = async (request, response) => {
     const cache = await caches.open(CACHE_NAME);
