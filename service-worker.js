64b8153dba214c3f037fddad3c74a7b5bcc69991
Wouter <w.vos@elitacwearable.com>
2025-02-23 17:44:04 +0100

    flex file select

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 
-const CACHE_NAME = "v7.23"; 
+const CACHE_NAME = "v7.24"; 
 
 const putInCache = async (request, response) => {
     const cache = await caches.open(CACHE_NAME);
