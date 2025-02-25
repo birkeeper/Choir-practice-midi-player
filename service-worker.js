7d700499be85a6dacda779b3cc7fd1278d8fe9e0
Wouter <w.vos@elitacwearable.com>
2025-02-25 20:27:24 +0100

    allow growing and shrinking

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 
-const CACHE_NAME = "v7.38"; 
+const CACHE_NAME = "v7.39"; 
 
 const putInCache = async (request, response) => {
     const cache = await caches.open(CACHE_NAME);
