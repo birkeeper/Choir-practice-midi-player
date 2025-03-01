528472feba573a156baa19e8dd1c901d46ef7b2b
Wouter <w.vos@elitacwearable.com>
2025-03-01 17:14:59 +0100

    more fine-tuning

--- midi_player.js
+++ midi_player.js
@@ -10,7 +10,7 @@ import { getPauseSvg, getPlaySvg, getFileOpenSvg } from './js/icons.js'
 import {MIDI} from "./libraries/spessasynth_lib/src/spessasynth_lib/midi_parser/midi_loader.js";
 
 
-const VERSION = "v1.2.3av"
+const VERSION = "v1.2.3aw"
 const DEFAULT_PERCUSSION_CHANNEL = 9; // In GM channel 9 is used as a percussion channel
 const ICON_SIZE_PX = 24; // size of button icons
 const MAINVOLUME = 1.5;
