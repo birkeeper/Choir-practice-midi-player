adfc3ab10ab4d5a4064630069f19a22f5c2eff0d
Wouter <w.vos@elitacwearable.com>
2025-05-29 18:54:52 +0200

    avoid copying incompatible settings in old cache

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 
 const SOUNDFONT_GM = "./soundfonts/GeneralUserGS.sf3"; // General Midi soundfont
 const SOUNTFONT_SPECIAL = "./soundfonts/Choir_practice.sf2"; //special soundfont
-const CACHE_NAME = "v7.130"; 
+const CACHE_NAME = "v7.131"; 
 
 const putInCache = async (request, response) => {
     const cache = await caches.open(CACHE_NAME);
@@ -60,7 +60,7 @@ const putInCache = async (request, response) => {
         // Get the latest cache name
         const latestCacheName = oldCacheNames[oldCacheNames.length - 1];
         
-        if (latestCacheName) {
+        if (latestCacheName && Number(latestCacheName)>7.0) {//settings of caches of versions <=7.0 are not compatible
           caches.open(latestCacheName).then(oldCache => {
             oldCache.keys().then(requests => {
               const settingsRequests = requests.filter(request => request.url.includes('/settings/'));
