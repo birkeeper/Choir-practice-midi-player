1fd24ee6d4fa62720b82089766ea3150f56167b2
Wouter <w.vos@elitacwearable.com>
2025-05-29 19:03:07 +0200

    retry

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 
 const SOUNDFONT_GM = "./soundfonts/GeneralUserGS.sf3"; // General Midi soundfont
 const SOUNTFONT_SPECIAL = "./soundfonts/Choir_practice.sf2"; //special soundfont
-const CACHE_NAME = "v7.131"; 
+const CACHE_NAME = "v7.132"; 
 
 const putInCache = async (request, response) => {
     const cache = await caches.open(CACHE_NAME);
@@ -59,8 +59,9 @@ const putInCache = async (request, response) => {
         const oldCacheNames = cacheNames.filter(name => name !== CACHE_NAME);
         // Get the latest cache name
         const latestCacheName = oldCacheNames[oldCacheNames.length - 1];
+        const latestCacheNumber = Number(latestCacheName);
         
-        if (latestCacheName && Number(latestCacheName)>7.0) {//settings of caches of versions <=7.0 are not compatible
+        if (latestCacheName && (latestCacheNumber>7.0)) {//settings of caches of versions <=7.0 are not compatible
           caches.open(latestCacheName).then(oldCache => {
             oldCache.keys().then(requests => {
               const settingsRequests = requests.filter(request => request.url.includes('/settings/'));
