0c040b374305dfb1e15daa6a35ebab1220550de7
Wouter <w.vos@elitacwearable.com>
2025-10-20 17:49:30 +0200

    Do not show files in history without lastOpened attribute

--- midi_player.js
+++ midi_player.js
@@ -6,7 +6,7 @@ import { getPauseSvg, getPlaySvg, getFileOpenSvg, getFileHistorySvg } from './js
 import { SOUNDFONT_GM, SOUNTFONT_SPECIAL } from "./constants.js";
 
 
-const VERSION = "v2.0.0aj"
+const VERSION = "v2.0.0ak"
 const DEFAULT_PERCUSSION_CHANNEL = 9; // In GM channel 9 is used as a percussion channel
 const ICON_SIZE_PX = 24; // size of button icons
 const MAINVOLUME = 1.5;
@@ -588,6 +588,8 @@ document.getElementById("history-label").innerHTML = getFileHistorySvg(ICON_SIZE
                 return; 
             }
             
+            if (!Object.hasOwn(item, "lastOpened")) {return;} //unlikely to have a blob stored in the cache, because it has been copied from a previous cache.
+
             const li = document.createElement('li');
             li.innerHTML = `<a class="dropdown-item">${item.midiName}</a>`;
             li.midiFileHash = `${item.midiFileHash}`;
