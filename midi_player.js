7b9940a42cbc885034b4e1aa03820b758faa70b2
Wouter <w.vos@elitacwearable.com>
2025-11-09 18:22:18 +0100

    playing state issues

--- midi_player.js
+++ midi_player.js
@@ -6,7 +6,7 @@ import { getPauseSvg, getPlaySvg, getFileOpenSvg, getFileHistorySvg } from './js
 import { SOUNDFONT_GM, SOUNTFONT_SPECIAL } from "./constants.js";
 
 
-const VERSION = "v2.0.1h"
+const VERSION = "v2.0.1i"
 const DEFAULT_PERCUSSION_CHANNEL = 9; // In GM channel 9 is used as a percussion channel
 const ICON_SIZE_PX = 24; // size of button icons
 const MAINVOLUME = 1.5;
@@ -534,7 +534,6 @@ const audioElement = document.createElement('audio');
                 audioElement.play()
                 .then(() => {
                     navigator.mediaSession.metadata = new MediaMetadata({title: `${settings.midiName}`});
-                    navigator.mediaSession.playbackState = "paused";
                     navigator.mediaSession.setActionHandler("pause", () => {
                         document.getElementById("pause-label").innerHTML = getPlaySvg(ICON_SIZE_PX);
                         context.suspend();
@@ -552,11 +551,13 @@ const audioElement = document.createElement('audio');
                         document.getElementById("pause-label").innerHTML = getPauseSvg(ICON_SIZE_PX);
                         context.resume();
                         seq.play(); // resume
+                        navigator.mediaSession.playbackState = "playing";
                     }
                     else {
                         document.getElementById("pause-label").innerHTML = getPlaySvg(ICON_SIZE_PX);
                         context.suspend();
                         seq.pause(); // pause
+                        navigator.mediaSession.playbackState = "paused";
                     }
                     audioElement.pause();
                 });
