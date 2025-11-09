031dcb9935cd2c06f2901b03e8f5aff006ef37ac
Wouter <w.vos@elitacwearable.com>
2025-11-09 17:49:20 +0100

    wait for audioElement to play

--- midi_player.js
+++ midi_player.js
@@ -6,7 +6,7 @@ import { getPauseSvg, getPlaySvg, getFileOpenSvg, getFileHistorySvg } from './js
 import { SOUNDFONT_GM, SOUNTFONT_SPECIAL } from "./constants.js";
 
 
-const VERSION = "v2.0.1f"
+const VERSION = "v2.0.1g"
 const DEFAULT_PERCUSSION_CHANNEL = 9; // In GM channel 9 is used as a percussion channel
 const ICON_SIZE_PX = 24; // size of button icons
 const MAINVOLUME = 1.5;
@@ -411,22 +411,24 @@ const audioElement = document.createElement('audio');
                 if ("mediaSession" in navigator) {
                     audioElement.src = './icons/10-seconds-of-silence.mp3'; //dummy audio element
                     audioElement.play()
-                    navigator.mediaSession.metadata = new MediaMetadata({title: `${settings.midiName}`});
-                    navigator.mediaSession.playbackState = "paused";
-                    navigator.mediaSession.setActionHandler("pause", () => {
-                        document.getElementById("pause-label").innerHTML = getPlaySvg(ICON_SIZE_PX);
-                        context.suspend();
-                        seq.pause(); // pause
+                    .then(() => {
+                        navigator.mediaSession.metadata = new MediaMetadata({title: `${settings.midiName}`});
                         navigator.mediaSession.playbackState = "paused";
+                        navigator.mediaSession.setActionHandler("pause", () => {
+                            document.getElementById("pause-label").innerHTML = getPlaySvg(ICON_SIZE_PX);
+                            context.suspend();
+                            seq.pause(); // pause
+                            navigator.mediaSession.playbackState = "paused";
+                        });
+                        navigator.mediaSession.setActionHandler("play", () => {
+                            document.getElementById("pause-label").innerHTML = getPauseSvg(ICON_SIZE_PX);
+                            context.suspend();
+                            seq.play(); // play
+                            navigator.mediaSession.playbackState = "playing";
+                        });
+                        navigator.mediaSession.setPositionState({duration: seq.duration})
+                        audioElement.pause();
                     });
-                    navigator.mediaSession.setActionHandler("play", () => {
-                        document.getElementById("pause-label").innerHTML = getPauseSvg(ICON_SIZE_PX);
-                        context.suspend();
-                        seq.play(); // play
-                        navigator.mediaSession.playbackState = "playing";
-                    });
-                    navigator.mediaSession.setPositionState({duration: seq.duration})
-                    audioElement.pause();
                 }
             });
 
