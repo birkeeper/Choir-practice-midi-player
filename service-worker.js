89b93e03e801fd5851c8efc9824ae1135eecda93
Birkeeper <birkeeper@gmail.com>
2026-02-25 21:37:06 +0100

    bugfix

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 
 const SOUNDFONT_GM = "./soundfonts/GeneralUserGS.sf3"; // General Midi soundfont
 const SOUNTFONT_SPECIAL = "./soundfonts/Choir_practice.sf2"; //special soundfont
-const CACHE_NAME = "v9.54"; 
+const CACHE_NAME = "v9.55"; 
 
 const putInCache = async (request, response) => {
     try {
@@ -346,7 +346,7 @@ async function handleSongRequest(request, songID) {
     	},
 		cancel(reason) {
 			console.log(`ReadableStream canceled, because: ${reason}`);
-			port.postmessage({type: 'cancel'});
+			port.postMessage({type: 'cancel'});
 			port.close();
 		}
     });
