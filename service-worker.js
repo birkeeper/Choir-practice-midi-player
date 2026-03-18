f9fb582ba885eb8588fb69f9f2257a69e89d6d78
Birkeeper <birkeeper@gmail.com>
2026-03-18 21:05:57 +0100

    update service worker

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 
 const SOUNDFONT_GM = "./soundfonts/GeneralUserGS.sf3"; // General Midi soundfont
 const SOUNTFONT_SPECIAL = "./soundfonts/Choir_practice.sf2"; //special soundfont
-const CACHE_NAME = "v9.63"; 
+const CACHE_NAME = "v9.64"; 
 
 const putInCache = async (request, response) => {
     try {
