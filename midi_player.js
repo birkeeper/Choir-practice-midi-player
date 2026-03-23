a2f40faeb5331aaedb983fa77b18f087623230bc
Birkeeper <birkeeper@gmail.com>
2026-03-23 09:41:47 +0100

    added debug printing of range request

--- midi_player.js
+++ midi_player.js
@@ -3,7 +3,7 @@ import { MIDI } from './libraries/spessasynth_core/index.js';
 import { getPauseSvg, getPlaySvg, getFileOpenSvg, getFileHistorySvg } from './js/icons.js';
 import { WAV_NROFCHANNELS, WAV_BITSPERSAMPLE, WAV_SAMPLERATE, WAV_HEADERSIZE } from "./constants.js";
 
-const VERSION = "v2.0.1ce"
+const VERSION = "v2.0.1cf"
 const DEFAULT_PERCUSSION_CHANNEL = 9; // In GM channel 9 is used as a percussion channel
 const ICON_SIZE_PX = 24; // size of button icons
 const MAXNROFRECENTFILES = 10; // Maximum number of recently opened files that can be stored in the cache
@@ -60,10 +60,14 @@ console.log("dedicated worker created");
 navigator.serviceWorker.addEventListener("message", (event) => {
     const { data, ports } = event;
     const portFromSW = ports && ports[0];
-    if (!data || !portFromSW) return;
+    if (!data) return;
 	if (data.type === 'AUDIO_RANGE_REQ') {
+		if (!portFromSW) return;
 		console.log(`received ${data.type} message in main.js`);
 		dedicatedWorker.postMessage(data, [portFromSW]);
+	}
+	else if (data.type === 'DEBUG') {
+		appendAlert(data.message);
 	}
 });
 
--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 
 const SOUNDFONT_GM = "./soundfonts/GeneralUserGS.sf3"; // General Midi soundfont
 const SOUNTFONT_SPECIAL = "./soundfonts/Choir_practice.sf2"; //special soundfont
-const CACHE_NAME = "v9.85"; 
+const CACHE_NAME = "v9.86"; 
 
 const putInCache = async (request, response) => {
     try {
@@ -193,9 +193,18 @@ const putInCache = async (request, response) => {
 	const url = new URL(event.request.url);
  	if (url.pathname.includes('/generatedWav/') && url.pathname.endsWith('.wav')) {
     	console.log(`received fetch for: ${url}`);
+		let client; //DEBUG
+		for (const clientItem of clientList) { //DEBUG
+    		if (clientItem.url.includes("midi_player.html")) { //DEBUG
+      			client = clientItem; //DEBUG
+			} //DEBUG
+		} //DEBUG
+		let debugStringArray; //DEBUG
 		for (const pair of event.request.headers.entries()) {
   			console.log(`${pair[0]}: ${pair[1]}`);
+			debugStringArray.push(`${pair[0]}: ${pair[1]}`); //DEBUG
 		}
+		client.PostMessage({type: "DEBUG", message: debugStringArray.join("\n")}); //DEBUG
 		const id = url.pathname.match(/\/generatedWav\/(.+)\.wav$/);
 		const split = id[1].split('_'); // format: songID_randomUUID
 		event.respondWith(handleSongRequest(event.request, split[0], split[1]));
