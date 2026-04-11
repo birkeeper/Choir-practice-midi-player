d7472ea9dd2ef21bc59197dd78824de43b117d5d
Birkeeper <birkeeper@gmail.com>
2026-04-11 17:43:01 +0200

    update notice

--- midi_player.js
+++ midi_player.js
@@ -3,7 +3,7 @@ import { MIDI } from './libraries/spessasynth_core/index.js';
 import { getPauseSvg, getPlaySvg, getFileOpenSvg, getFileHistorySvg } from './js/icons.js';
 import { WAV_NROFCHANNELS, WAV_BITSPERSAMPLE, WAV_SAMPLERATE, WAV_HEADERSIZE } from "./constants.js";
 
-const VERSION = "v2.0.1cv"
+const VERSION = "v2.0.1cw"
 const DEFAULT_PERCUSSION_CHANNEL = 9; // In GM channel 9 is used as a percussion channel
 const ICON_SIZE_PX = 24; // size of button icons
 const MAXNROFRECENTFILES = 10; // Maximum number of recently opened files that can be stored in the cache
@@ -32,6 +32,7 @@ if ("serviceWorker" in navigator) {
                             'warning', "update",
                             () => {
                                 console.log("Posting skipWaiting to service worker.");
+								appendAlert("Update installing... When the installation has finished, the app will be reloaded automatically",'warning', "update");
                                 installingWorker.postMessage({ type: 'skipWaiting'}); }
                         );
                     }
