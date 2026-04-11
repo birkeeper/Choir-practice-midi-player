b7005aa8c9cb9f6cc8b59096459f6441860a80f7
Birkeeper <birkeeper@gmail.com>
2026-04-11 18:48:05 +0200

    resolve issue with rejected audioElement.play(), because a .load() was called before resolvement.

--- midi_player.js
+++ midi_player.js
@@ -3,7 +3,7 @@ import { MIDI } from './libraries/spessasynth_core/index.js';
 import { getPauseSvg, getPlaySvg, getFileOpenSvg, getFileHistorySvg } from './js/icons.js';
 import { WAV_NROFCHANNELS, WAV_BITSPERSAMPLE, WAV_SAMPLERATE, WAV_HEADERSIZE } from "./constants.js";
 
-const VERSION = "v2.0.1cx"
+const VERSION = "v2.0.1cy"
 const DEFAULT_PERCUSSION_CHANNEL = 9; // In GM channel 9 is used as a percussion channel
 const ICON_SIZE_PX = 24; // size of button icons
 const MAXNROFRECENTFILES = 10; // Maximum number of recently opened files that can be stored in the cache
@@ -304,9 +304,8 @@ async function activateApplication(instruments)
 		}
 
 		// make a slider to set the playback rate
-		playbackRateInput.addEventListener('input',playbackRateCallback);
+		playbackRateInput.addEventListener('change',playbackRateCallback);
 		async function playbackRateCallback() {
-			//seq.playbackRate = playbackRateInput.value; // TO BE REPLACED with new code
 			playbackRateValue.textContent = `${Number(playbackRateInput.value).toFixed(2)}x`;
 			dedicatedWorker.postMessage({type: 'playbackRate', value: playbackRateInput.value});
 			let currentPlaybackRate = settings.playbackRate;
