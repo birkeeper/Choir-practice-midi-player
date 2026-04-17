718e1aaad9e8457293d2eec9b2efd40f8ee52662
Birkeeper <birkeeper@gmail.com>
2026-04-17 18:10:14 +0200

    bugfix

--- midi_player.js
+++ midi_player.js
@@ -3,7 +3,7 @@ import { MIDI } from './libraries/spessasynth_core/index.js';
 import { getPauseSvg, getPlaySvg, getFileOpenSvg, getFileHistorySvg, getForwardSvg, getBackwardSvg } from './js/icons.js';
 import { WAV_NROFCHANNELS, WAV_BITSPERSAMPLE, WAV_SAMPLERATE, WAV_HEADERSIZE } from "./constants.js";
 
-const VERSION = "v3.0.0rc17"
+const VERSION = "v3.0.0rc18"
 const DEFAULT_PERCUSSION_CHANNEL = 9; // In GM channel 9 is used as a percussion channel
 const ICON_SIZE_PX = 24; // size of button icons
 const MAXNROFRECENTFILES = 10; // Maximum number of recently opened files that can be stored in the cache
@@ -598,7 +598,6 @@ async function activateApplication(instruments)
 		audioElement.currentTime = currentTime / settings.playbackRate;
 		if (paused) { // first start it before pausing, else mediaSession element will not be shown
 			audioElement.pause();
-			audioElement.play()
 			if ("mediaSession" in navigator) { // else the mediaSession in the notification screen will be closed
 				navigator.mediaSession.metadata = new MediaMetadata({title: `${settings.midiName}`});
 				navigator.mediaSession.playbackState = "paused";
