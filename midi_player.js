24a391c1eff4b78e67d7bd61d1b3004b6b6cd0c0
birkeeper <birkeeper@gmail.com>
2026-05-06 21:41:49 +0200

    retry fix

--- midi_player.js
+++ midi_player.js
@@ -3,7 +3,7 @@ import { MIDI } from './libraries/spessasynth_core/index.js';
 import { getPauseSvg, getPlaySvg, getFileOpenSvg, getFileHistorySvg, getForwardSvg, getBackwardSvg } from './js/icons.js';
 import { WAV_NROFCHANNELS, WAV_BITSPERSAMPLE, WAV_SAMPLERATE, WAV_HEADERSIZE } from "./constants.js";
 
-const VERSION = "v3.0.0rc65"
+const VERSION = "v3.0.0rc66"
 const DEFAULT_PERCUSSION_CHANNEL = 9; // In GM channel 9 is used as a percussion channel
 const ICON_SIZE_PX = 24; // size of button icons
 const MAXNROFRECENTFILES = 10; // Maximum number of recently opened files that can be stored in the cache
@@ -625,10 +625,10 @@ async function activateApplication(instruments)
 		});
 		audioElement.addEventListener("timeupdate", () => {
 			if (!progressSlider.BeingDragged && audioElement.readyState >= 2) { // do not update the progress slider when it is dragged, or data is not available for the current playback position
-				progressSlider.value = Math.floor(audioElement.currentTime * settings.playbackRate);
-				currentTimeDisplay.textContent = formatTime(audioElement.currentTime * settings.playbackRate);
+				progressSlider.value = Math.floor(audioElement.currentTime * currentPlaybackRate);
+				currentTimeDisplay.textContent = formatTime(audioElement.currentTime * currentPlaybackRate);
 				if (("mediaSession" in navigator) && (audioElement.duration >= audioElement.currentTime)) {
-					navigator.mediaSession.setPositionState({ duration: settings.duration_s, position: audioElement.currentTime * settings.playbackRate });
+					navigator.mediaSession.setPositionState({ duration: settings.duration_s, position: audioElement.currentTime * currentPlaybackRate });
 				}
 			}
 		});
